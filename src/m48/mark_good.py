a49704631d14b3873b74db0af81836203248b181
hg <hg@localhost>
2014-07-02 18:06:07 +0200

    added and corrected for M48

---
+++ src/m48/mark_good.py
@@ -0,0 +1,49 @@
+'''
+Created on Oct 25, 2013
+
+@author: jwe <jweingrill@aip.de>
+'''
+
+def filename2objid(filename):
+    #science20101031A-0009EXP0010.png
+    
+    i = filename.find('science')
+    objid = filename[i+7:]
+    objid = objid.replace('EXP','-')
+    return objid[:19]
+
+def set_good(objid):
+    query = """UPDATE frames
+               SET good=True
+               WHERE objid like '%s';""" % objid
+    wifsip.execute(query)
+
+def set_bad(objid):
+    query = """UPDATE frames
+               SET good=False
+               WHERE objid like '%s';""" % objid
+    wifsip.execute(query)
+
+
+if __name__ == '__main__':
+    from glob import glob
+    from datasource import DataSource
+    
+    wifsip = DataSource(host = 'pina', database = 'wifsip', user = 'sro')
+    pngs = glob('/work2/jwe/stella/wifsip/m48/science*.png')
+    for p in pngs:
+        #print p
+        objid = filename2objid(p)
+        print objid
+        set_good(objid)
+
+    pngs = glob('/work2/jwe/stella/wifsip/m48/bad/science*.png')
+    for p in pngs:
+        #print p
+        objid = filename2objid(p)
+        print objid
+        set_bad(objid)
+
+
+    wifsip.commit()
+    wifsip.close()
