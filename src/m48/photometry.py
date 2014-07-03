5c9610ed153187bacb8badcea649748a2f128a60
hg <hg@localhost>
2014-07-03 17:10:56 +0200

    added default values on database creation, which fixed bug. added vmag and bmag to set to NULL when no data is available. calibration to Tycho manually.

--- src/m48/photometry.py
+++ src/m48/photometry.py
@@ -28,6 +28,7 @@ class Photometry(object):
             raise(ValueError)
         
         self.frames=[]
+        print 'filter %s' % self.filtercol
 
     def createtable(self):
         '''
@@ -39,29 +40,29 @@ class Photometry(object):
         CREATE TABLE m48stars(
          starid varchar(25),
          bv real,
-         vmag real,
+         vmag real default 0,
          vmag_err real,
-         bmag real,
+         bmag real default 0,
          bmag_err real,
          period real,
          period_err real,
          theta real,
          amp real,
          amp_err real,
-         nv integer,
-         nb integer,
+         nv integer default 0,
+         nb integer default 0,
          ra double precision,
          dec double precision,
          coord point,
          PRIMARY KEY (starid));
         GRANT SELECT ON m48stars TO public;
-        CREATE INDEX idx_m48stars_coords ON m48stars USING GIST (circle(coord,3.0/3600.));
+        CREATE INDEX idx_m48stars_coords ON m48stars USING GIST (circle(coord,1.0/3600.));
         """
         self.wifsip.execute(query)
         print "table 'm48stars' created"
 
     def getframes(self):
-        for field in ['C']: #['C','NW','NE','SW','SE']:
+        for field in ['C','NW','NE','SW','SE']:
             query = """SELECT object, objid, abs(corr)
              FROM frames
              WHERE object LIKE 'M 48 BVI %s'
@@ -75,12 +76,12 @@ class Photometry(object):
             if len(result)==0:
                 print 'no frames found!'
             for r in result: 
-                print '%s\t%s\t%.3f: ' % r,
+                print '%s\t%s\t%.3f: ' % r
                 objid = r[1]
                 self.filltable(objid)
-                
                 self.frames.append(objid)
             self.wifsip.dropview('phot1')
+        self.update_magnitudes()
         #print '\n'.join(self.frames)
 
     def filltable(self, objid):
@@ -93,38 +94,47 @@ class Photometry(object):
         result = self.wifsip.query(query)
         #... and inject them into the m48stars 
         stars = len(result)
-        if stars>800:
-            print '%d stars' % stars
+        if stars>400:
+            print '%5d stars: ' % stars,
+            oldstars = 0
+            newstars = 0 
             for r in result:
-                self.addstar(r[0], r[1], r[2], r[3])
+                ostars = self.addstar(r[0], r[1], r[2], r[3])
+                if ostars == 0:
+                    newstars += 1
+                else:
+                    oldstars += 1 
+            print '%5d old , %5d new' % (oldstars, newstars)
+            self.wifsip.commit()
         else: print 'not enough stars (%d)' % stars
             #value = '%s\t%f\t%f\t%f' % r
             #print value
+        
             
 
     def addstar(self, starid, mag, ra, dec):
         # identify by coordinates, if the star is already in table
-        query = """SELECT starid, (point(%f,%f)<->coord)*3600.
+        query = """SELECT starid
         FROM m48stars
-        WHERE circle(point(%f,%f),0)<@circle(coord,3.0/3600.)
+        WHERE circle(point(%f,%f),0)<@circle(coord,1.0/3600.)
         ORDER BY point(%f,%f)<->coord
-        LIMIT 1;""" % (ra, dec, ra, dec, ra, dec)
+        LIMIT 1;""" % (ra, dec, ra, dec)
         result = self.wifsip.query(query)
-        print result
+        oldstar = 0
         # if not: append new star
         if len(result)==0:
-            print 'new'
+            oldstar = 0
             if self.filtercol=='B':
                 query="""INSERT INTO m48stars (starid, bmag, nb, ra, dec, coord)
                 VALUES ('%s', %f, 1, %f, %f, point(%f,%f))""" % (starid, mag, ra, dec, ra, dec)
             elif self.filtercol=='V':
                 query="""INSERT INTO m48stars (starid, vmag, nv, ra, dec, coord)
                 VALUES ('%s', %f, 1, %f, %f, point(%f,%f))""" % (starid, mag, ra, dec, ra, dec)
-            self.wifsip.execute(query)
+            
         # if star exists: add up magnitudes, increase counter
         else:
+            oldstar = 1
             oldid = result[0][0]
-            print '= %s' % oldid
             if self.filtercol=='B':
                 query = """UPDATE m48stars
                 SET bmag = bmag + %f, nb = nb + 1
@@ -135,24 +145,61 @@ class Photometry(object):
                 SET vmag = vmag + %f, nv = nv + 1
                 WHERE starid = '%s';
                 """ % (mag, oldid)
-            self.wifsip.execute(query)
+        self.wifsip.execute(query)
+        return oldstar
     
     def update_magnitudes(self):
-        query = """UPDATE m48stars
-        SET bmag=bmag/nb
-        WHERE nb>1;
-        UPDATE m48stars
-        SET vmag=vmag/nv
-        WHERE nv>1;
-        UPDATE m48stars
-        SET bv=bmag-vmag;""" 
+        if self.filtercol=='B':
+            query = """UPDATE m48stars
+            SET bmag=bmag/nb
+            WHERE nb>1;
+            UPDATE m48stars
+            SET bmag=NULL
+            WHERE nb=0;"""
+        elif self.filtercol=='V':
+            query = """    
+            UPDATE m48stars
+            SET vmag=vmag/nv
+            WHERE nv>1;
+            UPDATE m48stars
+            SET vmag=NULL
+            WHERE nv=0;""" 
         self.wifsip.execute(query)   
 
+def make_cmd():
+    import pylab as plt
+    import numpy as np
+    from datasource import DataSource
+
+    wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
+    query = "UPDATE m48stars SET bv = (bmag-vmag)+(0.761837-0.060177);"
+    
+    wifsip.execute(query)
+    query = "SELECT vmag+0.060177, bv FROM m48stars WHERE NOT bv is NULL and nv>1 and nb>1;"
+    
+    data = wifsip.query(query)
+    wifsip.close()
+    vmag = np.array([d[0] for d in data])
+    bv = np.array([d[1] for d in data])
+    plt.scatter(bv,vmag, edgecolor='none', alpha=0.75, s=4, c='k')
+    
+    plt.ylim(22.0, 7.0)
+    plt.xlim(-0.2, 2.0)
+    plt.xlabel('B - V')
+    plt.ylabel('V [mag]')
+    plt.title('M 48')
+    plt.grid()
+    plt.savefig('/work2/jwe/m48/m48cmd.pdf')
+    #plt.show()
+    plt.close()
+
+
 if __name__ == '__main__':
-    p = Photometry(filtercol='V')
-    p.createtable()
-    p.getframes()
-        
-    p = Photometry(filtercol='B')
-    p.getframes()
-    p.update_magnitudes()
+    pv = Photometry(filtercol='V')
+    pv.createtable()
+    pv.getframes()
+         
+    pb = Photometry(filtercol='B')
+    pb.getframes()
+    
+    make_cmd()
