797eb73c20aa17e8627b730c5f5cbe0094b283cb
Joerg Weingrill <joerg.weingrill@aip.de>
2014-07-07 11:25:05 +0200

    added getrotframes ot fetch only rotational frames from pina

--- src/m48/getframes.py
+++ src/m48/getframes.py
@@ -22,5 +22,22 @@ def getframes(targetdir='/work2/jwe/stella/wifsip/m48'):
         print path+'/'+filename
         call(['scp', 'sro@pina.aip.de:'+path+'/'+filename+'.fitz ',targetdir])
 
+def getrotframes(targetdir='/work2/jwe/stella/wifsip/m48/rot'):
+    from datasource import DataSource
+    from subprocess import call
+
+    
+    wifsip = DataSource(host = 'pina', database = 'wifsip', user = 'sro')
+    query = """SELECT path, filename
+             FROM frames, science
+             WHERE object LIKE 'M 48 rot%%'
+              AND frames.objid = science.objid
+              order by frames.objid"""
+    tab = wifsip.query(query)
+    for path, filename in tab:
+        print path+'/'+filename
+        call(['scp', 'sro@pina.aip.de:'+path+'/'+filename+'.fitz ',targetdir])
+
+
 if __name__ == '__main__':
-    getframes()
+    getrotframes()
