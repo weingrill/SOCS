f0a660fb87be6f92d3ea2b9aa1fc635e8b4356cf
Joerg Weingrill <jweingrill@aip.de>
2014-10-02 15:25:13 +0200

    introduced command line arguments

--- src/m48/getframes.py
+++ src/m48/getframes.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Created on May 3, 2013
 
 @author: jwe <jweingrill@aip.de>
 
-get files from M 48BVI
+get files from M 48 BVI
 '''
-def getframes(targetdir='/work2/jwe/stella/wifsip/m48'):
+#TODO: argparse
+def getframes(obj, targetdir='/work2/jwe/stella/wifsip/m48'):
     from datasource import DataSource
     from subprocess import call
 
@@ -13,10 +16,12 @@ def getframes(targetdir='/work2/jwe/stella/wifsip/m48'):
     wifsip = DataSource(host = 'pina', database = 'wifsip', user = 'sro')
     query = """SELECT path, filename
              FROM frames, science
-             WHERE (filter LIKE 'V' OR FILTER LIKE 'B')
-              AND OBJECT LIKE 'M 48 BVI %%'
+             WHERE (filter LIKE 'V')
+              AND OBJECT LIKE '%s%%'
               AND frames.objid = science.objid
-              order by frames.objid"""
+              AND fwhm_image < 3
+              AND backgrnd < 500
+              order by frames.objid""" % obj
     tab = wifsip.query(query)
     for path, filename in tab:
         print path+'/'+filename
@@ -40,4 +45,8 @@ def getrotframes(targetdir='/work2/jwe/stella/wifsip/m48/rot'):
 
 
 if __name__ == '__main__':
-    getrotframes()
+    import argparse
+    parser = argparse.ArgumentParser(description='M48 analysis')
+    parser.add_argument('--clear', action='store_true', help='clear periods')
+    parser.add_argument('--object', dest='obj' )
+    getframes()
