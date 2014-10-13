75566190a3af6354147bf2c9f9844d21b7ca7052
Joerg Weingrill <jweingrill@aip.de>
2014-10-13 14:57:07 +0200

    added config, argparse, do_cmd, do_rot

--- src/m67/planwifsip.py
+++ src/m67/planwifsip.py
@@ -1,34 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Created on Apr 12, 2013
 
 @author: jwe <jweingrill@aip.de>
 '''
-
+import config
+from opencluster import OpenCluster
 def do_uvby(transfer=False, plot=False):
-    from opencluster import OpenCluster
-    
     m67 = OpenCluster(objectname='M 67', 
                           uname='M 67 uvby', 
                           obsmode='uvby')
     m67.title = 'SOCS'
     m67.abstract = 'Photometric monitoring of open stellar clusters'
     m67.tofile('/work2/jwe/m67')
-    if transfer:
-        m67.transfer()
+    if transfer: m67.transfer()
 
-    if plot:
-        import matplotlib.pyplot as plt
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_aspect(1.)
-        m67.tycho()
-        m67.plot()
-        plt.show()
     
 def do_hahb(transfer=False):
-    from opencluster import OpenCluster
-    
     m67 = OpenCluster(objectname='M 67', 
                           uname='M 67 hahb', 
                           obsmode='hahb')
@@ -38,9 +27,47 @@ def do_hahb(transfer=False):
     if transfer:
         m67.transfer()
 
+def do_rot(transfer=False):
+
+    m67 = OpenCluster(objectname='M 67', uname='M 67 rot', obsmode='rot')
+    m67.title = 'SOCS'
+    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67_subframes = m67.plan_wifsip(nfields=4)
+
+    
+    for sf in m67_subframes:
+        print sf.uname, sf.duration
+        sf.tofile(config.projectpath)
+        if transfer: sf.transfer()
+
+def do_cmd(transfer=False):
+    m67 = OpenCluster(objectname='M 67', uname='M 67 BVR', obsmode='BVR')
+    m67.title = 'SOCS'
+    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67_subframes = m67.plan_wifsip(nfields=5)
+    for sf in m67_subframes:
+        print sf.uname, sf.duration
+        
+        sf.tofile(config.projectpath)
+        if transfer: sf.transfer()
         
 if __name__ == '__main__':
-    import matplotlib
-    matplotlib.use('WXAgg')
     do_uvby(transfer=True)
     do_hahb(transfer=True)
+    import argparse
+
+    parser = argparse.ArgumentParser(description='NGC1528 WiFSIP schedule')
+    parser.add_argument('-rot', action='store_true', help='rot observations')
+    parser.add_argument('-cmd', action='store_true', help='CMD observations')
+    parser.add_argument('-uvby', action='store_true', help='uvby observations')
+    parser.add_argument('-hahb', action='store_true', help='hahb observations')
+    parser.add_argument('--transfer', action='store_true', 
+                        help='transfert files via sftp to stella')
+
+    args = parser.parse_args()
+    
+    if args.hahb: do_hahb(transfer=args.transfer)
+    if args.uvby: do_uvby(transfer=args.transfer)
+    if args.rot: do_rot(transfer=args.transfer)
+    if args.cmd: do_cmd(transfer=args.transfer)
+    
