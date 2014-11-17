3b219ebc2bf68e2bb28caa04fc25fe9f9b0b9f06
Joerg Weingrill <jweingrill@aip.de>
2014-11-17 12:16:00 +0100

    multiple lightcurves for Sydney's eval poster

--- src/m48/posterlightcurve.py
+++ src/m48/posterlightcurve.py
@@ -6,12 +6,21 @@ Created on Oct 14, 2014
 @author: jwe
 '''
 
-if __name__ == '__main__':
-    from m48star import M48Star
-    import pylab as plt
-    from functions import sigma_clip
-    import numpy as np            
-    star = M48Star('20140303A-0074-0013#1855')
+from m48star import M48Star
+import pylab as plt
+from functions import sigma_clip
+import numpy as np
+
+starids = ['20140301A-0071-0013#125',
+           '20140303A-0074-0013#1855',
+           '20140301A-0071-0013#1218',
+           '20140301A-0071-0013#1316',
+           '20140306A-0097-0013#732',
+           '20140307A-0000-0013#334']
+
+plt.subplots(3, 2)
+for starid in starids:            
+    star = M48Star(starid)
     try:
         t, m, e = star.lightcurve()
     except TypeError:
@@ -22,15 +31,21 @@ if __name__ == '__main__':
     mean = np.mean(m)
     m -= mean
     mean = 0.0
+    
+    ax = plt.subplot(3,2,starids.index(starid))
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
     plt.hlines(mean,min(t),max(t),linestyle='--')
     plt.xlim(min(t),max(t))
     plt.plot(t,m,'gray')
     plt.scatter(t, m, edgecolor='none', facecolor='k', s=20)
     ylim=[max(m)+0.01, min(m)-0.01]
-    #plt.text(1, ylim[1]+0.0025, star['tab'], fontsize=12)
+    plt.text(1, ylim[1]+0.0025, star['tab'], fontsize=12)
     plt.ylim(ylim[1],ylim[0])
-    plt.xlabel('days')
-    plt.ylabel('V mag')
-    #plt.show()
-    plt.savefig('/work1/jwe/Dropbox/Documents/Poster/AIP2014/m48_lc332.eps')
+    #plt.xlabel('days')
+    #plt.ylabel('V mag')
+plt.tight_layout()    
+#plt.show()
+plt.savefig('/work2/jwe/m48/plots/lightcurves.pdf')
+plt.savefig('/work2/jwe/m48/plots/lightcurves.png')
     
