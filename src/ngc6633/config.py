8a4f16abafca50c20294ee91a84d9b6b519a0006
Joerg Weingrill <jweingrill@aip.de>
2015-01-08 11:35:28 +0100

    added config file

---
+++ src/ngc6633/config.py
@@ -0,0 +1,21 @@
+'''
+Created on Oct 7, 2014
+
+@author: jwe
+
+global configurationfile for ngc 6633
+'''
+import os
+
+projectpath = '/work2/jwe/ngc6633/'
+
+resultpath = projectpath+'results/'
+plotpath = projectpath+'plots/'
+datapath = projectpath+'data/'
+lightcurvespath = projectpath+'lightcurves/'
+
+if not os.path.exists(projectpath): os.mkdir(projectpath)
+if not os.path.exists(resultpath): os.mkdir(resultpath)
+if not os.path.exists(plotpath): os.mkdir(plotpath)
+if not os.path.exists(datapath): os.mkdir(datapath)
+if not os.path.exists(lightcurvespath): os.mkdir(lightcurvespath)
