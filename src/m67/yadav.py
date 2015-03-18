adea2216c6287e12d3f2b3e174e0d6ddb43e93fd
Joerg Weingrill <jweingrill@aip.de>
2015-03-18 09:13:26 +0100

    added yadav.py (initial commit)

---
+++ src/m67/yadav.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on Feb 5, 2015
+
+@author: Joerg Weingrill <jweingrill@aip.de>
+'''
+
+class Yadav(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.data = []
+        from datasource import DataSource
+        self.wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
+        
+    def fromfile(self, filename=None):
+        import pyfits
+        
+        hdulist = pyfits.open(filename)
+        self.data = hdulist[1].data
+        self.keys = []
+        for col in hdulist[1].columns:
+            self.keys.append(col.name)
+        hdulist.close()
+    
+    def todatabase(self):
+        import numpy as np
+        for d in self.data:
+            print d['seq']
+            record = {}
+            for key in self.keys:
+                record[key] = d[key]
+		if np.isnan(record[key]):
+                    record[key] = 'NULL'
+            query = """INSERT INTO m67 (seq, ra, dec, vmag, bmag, icmag, pmra, pmdec, pmb, hrv) 
+VALUES (%(Seq)d, %(RAJ2000)f, %(DEJ2000)f, %(Vmag)s, %(Bmag)s, %(Icmag)s, %(pmRA)f, %(pmDE)f, %(Pmb)f, %(HRV)s);""" % record
+            self.wifsip.execute(query, commit=False)
+            
+        self.wifsip.commit()        
+
+y = Yadav()
+y.fromfile('/home/jwe/Dropbox/Public/Yadav.fit')
+y.todatabase()        
