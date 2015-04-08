86c2751e8cf5f3c1528f393d702313f094940755
Joerg Weingrill <jweingrill@aip.de>
2015-04-08 17:26:07 +0200

    planwifsip: all classes named different

--- src/m67/planwifsip.py
+++ src/m67/planwifsip.py
@@ -10,78 +10,78 @@ from opencluster import OpenCluster
 from clustergroup import ClusterGroup
 
 def do_uvby(transfer=False, plot=False):
-    m67 = OpenCluster(objectname='M 67', 
+    m67uvby = OpenCluster(objectname='M 67', 
                           uname='M 67 uvby', 
                           obsmode='uvby')
-    m67.title = 'SOCS'
-    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67uvby.title = 'SOCS'
+    m67uvby.abstract = 'Photometric monitoring of open stellar clusters'
 
-    m67_subframes = m67.plan_wifsip(nfields=5)
-    m67_group = ClusterGroup(m67)
+    m67uvby_subframes = m67uvby.plan_wifsip(nfields=5)
+    m67uvby_group = ClusterGroup(m67uvby)
     
-    for sf in m67_subframes:
+    for sf in m67uvby_subframes:
         print sf.uname
         sf.tofile(config.projectpath)
-        m67_group.add_daughter(sf.uname)
+        m67uvby_group.add_daughter(sf.uname)
         if transfer: sf.transfer()
+    m67uvby_group.tofile(config.projectpath)
     if transfer: 
-        m67_group.tofile(config.projectpath)
-        m67_group.transfer()
+        m67uvby_group.transfer()
  
     
 def do_hby(transfer=False):
-    m67 = OpenCluster(objectname='M 67', 
+    m67hby = OpenCluster(objectname='M 67', 
                           uname='M 67 Hby', 
                           obsmode='Hby')
-    m67.title = 'SOCS'
-    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67hby.title = 'SOCS'
+    m67hby.abstract = 'Photometric monitoring of open stellar clusters'
 
-    m67_subframes = m67.plan_wifsip(nfields=5)
-    m67_group = ClusterGroup(m67)
+    m67hby_subframes = m67hby.plan_wifsip(nfields=5)
+    m67hby_group = ClusterGroup(m67hby)
     
-    for sf in m67_subframes:
+    for sf in m67hby_subframes:
         print sf.uname
         sf.tofile(config.projectpath)
-        m67_group.add_daughter(sf.uname)
+        m67hby_group.add_daughter(sf.uname)
         if transfer: sf.transfer()
+    m67hby_group.tofile(config.projectpath)
     if transfer: 
-        m67_group.tofile(config.projectpath)
-        m67_group.transfer()
+        m67hby_group.transfer()
  
 def do_rot(transfer=False):
 
-    m67 = OpenCluster(objectname='M 67', uname='M 67 rot', obsmode='rot')
-    m67.title = 'SOCS'
-    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67rot = OpenCluster(objectname='M 67', uname='M 67 rot', obsmode='rot')
+    m67rot.title = 'SOCS'
+    m67rot.abstract = 'Photometric monitoring of open stellar clusters'
 
-    m67_subframes = m67.plan_wifsip(nfields=4)
-    m67_group = ClusterGroup(m67)
+    m67rot_subframes = m67rot.plan_wifsip(nfields=4)
+    m67rot_group = ClusterGroup(m67rot)
     
-    for sf in m67_subframes:
+    for sf in m67rot_subframes:
         print sf.uname
         sf.tofile(config.projectpath)
-        m67_group.add_daughter(sf.uname)
+        m67rot_group.add_daughter(sf.uname)
         if transfer: sf.transfer()
+    m67rot_group.tofile(config.projectpath)
     if transfer: 
-        m67_group.tofile(config.projectpath)
-        m67_group.transfer()
+        m67rot_group.transfer()
  
 def do_cmd(transfer=False):
-    m67 = OpenCluster(objectname='M 67', uname='M 67 BVR', obsmode='BVR')
-    m67.title = 'SOCS'
-    m67.abstract = 'Photometric monitoring of open stellar clusters'
+    m67cmd = OpenCluster(objectname='M 67', uname='M 67 BVR', obsmode='BVR')
+    m67cmd.title = 'SOCS'
+    m67cmd.abstract = 'Photometric monitoring of open stellar clusters'
 
-    m67_subframes = m67.plan_wifsip(nfields=5)
-    m67_group = ClusterGroup(m67)
+    m67cmd_subframes = m67cmd.plan_wifsip(nfields=5)
+    m67cmd_group = ClusterGroup(m67cmd)
     
-    for sf in m67_subframes:
+    for sf in m67cmd_subframes:
         print sf.uname
         sf.tofile(config.projectpath)
-        m67_group.add_daughter(sf.uname)
+        m67cmd_group.add_daughter(sf.uname)
         if transfer: sf.transfer()
+    m67cmd_group.tofile(config.projectpath)
     if transfer: 
-        m67_group.tofile(config.projectpath)
-        m67_group.transfer()
+        m67cmd_group.transfer()
         
 if __name__ == '__main__':
     import argparse
