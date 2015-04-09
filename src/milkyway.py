1ab44f13b9515ca744db7a1272928feb8fffecb9
Joerg Weingrill <jweingrill@aip.de>
2015-04-09 14:56:55 +0200

    milkyway: Contructor split up in subroutines: load, query, make and save plot now has smoothing function option for magnitudes instead of pure number densities

--- src/milkyway.py
+++ src/milkyway.py
@@ -13,42 +13,94 @@ class MilkyWay(object):
     '''
 
 
-    def __init__(self):
+    def __init__(self, filename, magnitudes=False):
         '''
         Constructor
         '''
+        try:
+            self._load(filename)
+        except IOError:
+            print 'loading failed'
+            self.query()
+            self.make(magnitudes)
+            self._save(filename)
+        
+    
+    def query(self):
         from datasource import DataSource
+        wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
+        query = """SELECT radeg, dedeg, vt from tycho where vt<=15.193"""
+        self.data = wifsip.query(query)
+        print 'fetched %d stars' % len(self.data)
         
-        self.wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
-        query = """SELECT radeg, dedeg, vt from tycho WHERE vt<=15.193"""
-        result = self.wifsip.query(query)
-        self.x = np.array([r[0] for r in result])
-        self.y = np.array([r[1] for r in result])
-        self.z = np.zeros([180,360])
-        for ra, dec, vt in result:
-            x = np.trunc(ra)
-            y = np.trunc(dec+90)
-            # faintest star in tycho has 15.193 in vt
-            z = 10.0**((15.193-vt)/2.5)
-            self.z[y,x] += z
-        self.z = -2.5*np.log10(self.z)
-        
-        np.save('/work2/jwe/tychomap', self.z)
-        
-    def plot(self):
+    def make(self, magnitudes=False):
+        print 'filling matrix'
+        self.z = np.zeros([180, 360])
+        if magnitudes:
+            for ra, dec, vt in self.data:
+                x = np.trunc(ra)
+                y = np.trunc(dec+90)
+                # faintest star in tycho has 15.193 in vt
+                z = 10.0**((15.193-vt)/2.5)
+                try:
+                    self.z[y,x] += z
+                except ValueError:
+                    print type(z),z
+            self.z = -2.5*np.log10(self.z+1.0)
+            self.z += 15.193
+        else:
+            for ra, dec in zip(self.x,self.y):
+                x = np.trunc(ra)
+                y = np.trunc((dec+90))
+                self.z[y,x] += 1
+    
+    def _load(self, filename='/work2/jwe/tychomap.npy'):
+        print 'loading matrix'
+        self.z = np.load(filename)    
+    
+    def _save(self, filename='/work2/jwe/tychomap.npy'):
+        print 'saving matrix'
+        np.save(filename, self.z)
+
+    def plot(self, show=True):
         """
         taken from http://matplotlib.org/1.4.3/examples/pylab_examples/contour_demo.html
+        http://wiki.scipy.org/Cookbook/SignalSmooth
         """
-        import matplotlib
-        #import numpy as np
-        import matplotlib.cm as cm
-        import matplotlib.mlab as mlab
+        def gauss_kern(size, sizey=None):
+            """ Returns a normalized 2D gauss kernel array for convolutions """
+            from scipy import mgrid
+            size = int(size)
+            if not sizey:
+                sizey = size
+            else:
+                sizey = int(sizey)
+            x, y = mgrid[-size:size+1, -sizey:sizey+1]
+            g = np.exp(-(x**2/float(size)+y**2/float(sizey)))
+            return g / g.sum()
+        
+        def blur_image(im, n, ny=None) :
+            """ blurs the image by convolving with a gaussian kernel of typical
+                size n. The optional keyword argument ny allows for a different
+                size in the y direction.
+            """
+            from scipy import signal
+            g = gauss_kern(n, sizey=ny)
+            improc = signal.convolve(im,g, mode='same')
+            return(improc)
         import matplotlib.pyplot as plt
         
-        plt.figure()
-        CS = plt.contour(self.z)
-        plt.show()
+        if show: plt.figure()
+        #x = np.arange(0,360)
+        x = np.arange(0, 24, 1./15)
+        y = np.arange(-90, 90)
+        print 'generating contour'
+        z = blur_image(self.z, 9)
+        print z.shape
+        CS = plt.contour(x,y, z)
+        if show: plt.show()
+        return CS
 
 if __name__ == '__main__':
-    mw = MilkyWay()
+    mw = MilkyWay('/work2/jwe/milkyway.npy', magnitudes=True)
     mw.plot()
