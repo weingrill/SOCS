4ededeb10c998ed6931c3ba4a171db18cd2d38fd
Joerg Weingrill <jweingrill@aip.de>
2015-09-16 12:11:10 +0200

    cdstable: added update_coords, database with global config

--- src/m48/cdstable.py
+++ src/m48/cdstable.py
@@ -18,15 +18,17 @@ class CDSTable(object):
         Constructor
         '''
         self.datastruct = []
-    def savetable2(self):
+
         from datasource import DataSource
-        wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
+        self.wifsip = DataSource(database=config.dbname, user=config.dbuser, host=config.dbhost)
+        
+    def savetable2(self):
         query = """SELECT tab, vmag, bv, p_fin, e_pfin, amp, amp_err, member, 
             simbad, provisional
             FROM m48stars 
             WHERE good
             ORDER BY vmag ;"""
-        self.data = wifsip.query(query)
+        self.data = self.wifsip.query(query)
         d0 =  self.data[0]
         print d0
         #Id   Vmag   B-V  P     P_er amp   amp_e M P Simbad
@@ -65,14 +67,12 @@ class CDSTable(object):
         from numpy import sqrt
         from astropy import units as u
         from astropy.coordinates import SkyCoord
-        from datasource import DataSource
-        wifsip = DataSource(database='wifsip', user='sro', host='pina.aip.de')
         
         query = """SELECT tab, vmag, vmag_err, bv, bmag_err, ra, dec, member, simbad
             FROM m48stars 
             WHERE not bv IS NULL
             ORDER BY vmag;"""
-        data = wifsip.query(query)
+        data = self.wifsip.query(query)
         f = open(config.resultpath+'table_appendix.dat','wt')
         for d in data:
             #print d
@@ -104,7 +104,80 @@ class CDSTable(object):
                 print d
         f.close()
 
+    def update_coords(self, filtercol = 'V'):
+        import numpy as np
+        from tools import log
+        query = """SELECT starid, ra, dec 
+        FROM m48stars 
+        WHERE NOT bv IS NULL AND coord IS NULL ORDER BY tab;"""
+        result = self.wifsip.query(query)
+        stars = []
+        for starid, ra, dec in result:
+            stars.append({'starid': starid, 'ra': ra, 'dec': dec})
+            
+        for star in stars:
+            #print star['NUMBER'],star['MAG_ISO'], 
+            params = {'ra': star['ra'],
+                      'dec': star['dec'],
+                      'filtercol': filtercol}
+            query = """SELECT alphawin_j2000, deltawin_j2000 
+                FROM phot, frames
+                WHERE object like 'M 48 %%'
+                AND phot.objid = frames.objid
+                AND filter = '%(filtercol)s'
+                AND circle(phot.coord,0) <@ circle(point(%(ra).11f,%(dec).11f), 0.3/3600.0)
+                AND flags<5;""" % params
+            result = self.wifsip.query(query)
+            #print result
+            if len(result) == 0:
+                print '%-25s: no data' % star['starid']
+                query = """UPDATE m48stars 
+                SET coord = NULL
+                WHERE starid = '%s';""" % star['starid']
+                self.wifsip.execute(query)
+                continue
+            elif len(result)==1:
+                print '%-25s: not enough data' % star['starid']
+                query = """UPDATE m48stars 
+                SET coord = NULL
+                WHERE starid = '%s';""" % star['starid']
+                self.wifsip.execute(query)
+                continue
+            elif len(result)>1:
+                ra = np.array([r[0] for r in result])
+                dec = np.array([r[1] for r in result])
+                
+                mean_ra = np.mean(ra)
+                mean_dec = np.mean(dec)
+                std_ra = np.std(ra)
+                std_dec = np.std(dec)
+                # remove outliers
+                std = np.sqrt(std_ra**2 + std_dec**2)
+                dist = np.sqrt((ra - mean_ra)**2 + (dec - mean_dec)**2) 
+                i = np.where(dist < 2*std)
+                mean_ra = np.mean(ra[i])
+                mean_dec = np.mean(dec[i])
+                std_ra = np.std(ra[i])
+                std_dec = np.std(dec[i])
+                n = len(ra[i])
+            params['starid'] = star['starid']
+            params['ra'] = mean_ra
+            params['dec'] = mean_dec
+            params['n'] = n
+            
+            params['sra'] = std_ra*3600.0
+            params['sdec'] = std_dec*3600.0
+            query = """UPDATE m48stars 
+            SET coord = point(%(ra).11f,%(dec).11f)
+            WHERE starid = '%(starid)s';""" % params
+            #query = query.replace('nan', 'NULL')
+            self.wifsip.execute(query)
+            log(config.projectpath+'coords1.log','%(starid)-25s, %(ra).6f, %(dec).6f, %(sra).2f, %(sdec).2f, %(n)3d' % params)
+        self.wifsip.commit()    
+        
+
 if __name__ == '__main__':
     cdst = CDSTable() 
-    cdst.savetable2()
-    cdst.saveappendix()
+    cdst.update_coords()
+    #cdst.savetable2()
+    #cdst.saveappendix()
