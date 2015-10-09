d51b602d107f5ab5c9e4c08c104e01116149a7bf
Joerg Weingrill <jweingrill@aip.de>
2015-10-09 18:59:20 +0200

    importref: todatabase implemented

--- src/m67/importref.py
+++ src/m67/importref.py
@@ -18,7 +18,9 @@ class ImportRef(object):
         '''
         Constructor
         '''
-        pass
+        self.data = []
+        from datasource import DataSource
+        self.wifsip = DataSource(database=config.dbname, user=config.dbuser, host=config.dbhost)
     
     def loadfromfile(self, filename=''):
         """
@@ -43,11 +45,46 @@ class ImportRef(object):
          126-133  F8.4  mag   e_Hb        ?=9.99  error in Hbeta
          135-136  A2    ---     Mem       [M NM] Membership label
         """
-        names = ['Seq','RAh','RAm','RAs','DEd','DEm','DEs',
+        self.names = ['Seq','RAh','RAm','RAs','DEd','DEm','DEs',
                  'xpos','ypos','b-y','e_b-y','Vmag','e_Vmag','m1','e_m1',
                  'c1','e_c1','Hb','e_Hb','Mem']        
-        self.data = np.genfromtxt(filename, dtype=None, names=names)
-        print self.data[0]
+        print filename
+        self.data = np.genfromtxt(filename, dtype=None, names=self.names, 
+                                  missing_values=('99.990', '9.9900'))
+        
+        
+    def todatabase(self):
+        import StringIO
+        from astropy.coordinates import SkyCoord  # High-level coordinates
+        import astropy.units as u
+        vallines = []
+        for d in self.data:
+            params = dict(zip(self.names,d))
+            radec = '%(RAh)s %(RAm)s %(RAs)s %(DEd)s %(DEm)s %(DEs)s' % params
+            c = SkyCoord(radec, unit=(u.hourangle, u.deg))  # @UndefinedVariable
+            params['ra'] = c.ra.deg
+            params['dec'] = c.dec.deg
+            params['starid'] = 'M67_BGJ%04d' % params['Seq']
+            params['source'] = '2007A&A...470..585B'
+            if params['b-y'] == 99.990: params['b-y'] = np.nan
+            if params['m1'] == 99.990: params['m1'] = np.nan
+            if params['c1'] == 99.990: params['c1'] = np.nan
+            if params['Hb'] == 99.990: params['Hb'] = np.nan
+            try:
+                vallines.append('%(starid)s\t%(ra).7f\t%(dec).7f\t%(b-y).3f\t%(m1).3f\t%(c1).3f\t%(Hb).3f\t%(source)s' % params)
+            except TypeError:
+                print params
+        values = '\n'.join(vallines)     
+        # replace NULL values with \N
+        values = values.replace('nan','\\N')
+        print values
+        f = StringIO.StringIO(values)
+        self.wifsip.cursor.copy_from(f,'referencestars',columns=('starid','ra','dec','"b-y"','m1','c1','beta','source'))
+        self.wifsip.commit()
+        self.wifsip.close()
+        
+
         
 ir = ImportRef()
 ir.loadfromfile(config.datapath+'table3.dat.gz')
+ir.todatabase()
