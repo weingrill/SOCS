b6466e764112049003873a9e655da08a81745565
Joerg Weingrill <jweingrill@aip.de>
2016-03-24 18:07:01 +0100

    calstromgren: added calbeta and leastsqcal methods

--- src/m67/calstromgren.py
+++ src/m67/calstromgren.py
@@ -43,11 +43,8 @@ class ReferenceStars(object):
         #for c in columns: print c
         dtypes = ['|S11','f4','f4','f4','f4','f4','f4']
         for c in columns:
-            print c
             dtypes.append(type(d0[c]))
-        #dtypes = [type(d) for d in data[0].values()]
         
-        for c, d in zip(columns, dtypes): print c,d
         arraydata = []
         for star in data:
             arraydata.append(tuple(star))
@@ -79,8 +76,8 @@ class CalStromgren(object):
         self.vframe = Frame('20151126A-0008-0007')
         self.uframe = Frame('20151126A-0008-0008')
         
-        self.hbnframe = Frame('20151127A-0011-0004')
-        self.hbwframe = Frame('20151127A-0011-0003')
+        self.hbnframe = Frame('20140227A-0011-0006') #'20151127A-0011-0004'
+        self.hbwframe = Frame('20140227A-0011-0003') #'20151127A-0011-0003'
            
         
     def loadref(self):
@@ -110,6 +107,87 @@ class CalStromgren(object):
         compfluxes[refidx] = fluxes[bidx]
         
         return compfluxes
+    
+    def calbeta(self):
+        cbeta = self.reference.stars['beta']
+        cby = self.reference.stars['b-y']
+        
+        hbn = self.refmatch(self.hbnframe)
+        hbw = self.refmatch(self.hbwframe)
+        
+        # convert to magnitudes
+        obeta = -2.5*np.log10(hbn/hbw)
+        
+        # remove NaN values
+        i = np.where(np.isfinite(obeta-cbeta))[0]
+
+        cbeta = cbeta[i]
+        obeta = obeta[i]
+        cby   = cby[i]
+        
+        ones = np.ones(len(cbeta))
+        
+        A = np.vstack([ones, obeta, cby]).T
+        c, kbeta, kby = np.linalg.lstsq(A, cbeta)[0]
+        print 'c = %.4f; kbeta = %.4f; kby = %.4f' %(c, kbeta, kby)
+        
+        o1beta = kbeta * obeta + kby * cby + c
+        #betaoffset = np.nanmean(obeta-cbeta)
+        
+        #plt.axhline(betaoffset, linestyle='--', color='g')
+        plt.plot(cby, obeta, 'bo', alpha=0.5)
+        plt.plot(cby, o1beta, 'ro', alpha=0.5)
+        plt.plot(cby, cbeta, 'go', alpha=0.5)
+        
+        plt.xlabel('beta')
+        plt.ylabel('o - c')
+        plt.grid()
+        
+        plt.show()
+    
+    def leastsqcal(self):
+        cby = self.reference.stars['b-y']
+        m1 = self.reference.stars['m1']
+        c1 = self.reference.stars['c1']
+        
+        u = self.refmatch(self.uframe)
+        v = self.refmatch(self.vframe)
+        b = self.refmatch(self.bframe)
+        y = self.refmatch(self.yframe)
+        
+        i = np.where(np.isfinite(u+v+b+y+cby+m1+c1))[0]
+        
+        cby = cby[i]
+        m1 = m1[i]
+        c1 = c1[i]
+        u = -2.5*np.log10(u[i])
+        v = -2.5*np.log10(v[i])
+        b = -2.5*np.log10(b[i])
+        y = -2.5*np.log10(y[i])
+        
+        ones = np.ones(len(cby))
+        
+        A = np.vstack([ones, b, y]).T
+        c, kb, ky = np.linalg.lstsq(A, cby)[0]
+        print kb, ky, c
+        
+        oby = kb*b + ky*y +c
+        
+        A = np.vstack([ones, v, b, y]).T
+        c, kv, kb, ky = np.linalg.lstsq(A, m1)[0]
+        print 'm1', kv, kb, ky, c
+        om1 = kv*v + kb*b + ky*y + c
+        
+        c, ku, kv, kb = np.linalg.lstsq(A, c1)[0]
+        print 'c1', ku, kv, kb, c
+        oc1 = ku*u + kv*v + kb*b + c
+        
+        plt.plot(cby, oby, 'bo')
+        plt.plot(m1, om1, 'mo')
+        plt.plot(c1, oc1, 'co')
+        plt.plot([0.0,1.0],[0.0,1.0], linestyle='--')
+        plt.grid()
+        plt.show()
     
     def match(self):
         
@@ -167,7 +245,7 @@ class CalStromgren(object):
 
         ax4 = fig.add_subplot(414)
         plt.axhline(betaoffset, linestyle='--', color='g')
-        ax4.scatter(cbeta, obeta-cbeta, edgecolor='none', alpha=0.5)
+        ax4.scatter(cbeta, obeta, edgecolor='none', alpha=0.5)
         ax4.set_xlabel('beta')
         ax4.set_ylabel('o - c')
         plt.grid()
@@ -179,8 +257,8 @@ class CalStromgren(object):
         
 if __name__ == '__main__':
     cs = CalStromgren()
-    #ref = refstars()
-    print cs.reference.stars['starid']
-    cs.match()
-    #print ref['starid']
+    #cs.match()
+    cs.calbeta()
+    #cs.leastsqcal()
+    
     
