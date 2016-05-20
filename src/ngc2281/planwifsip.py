d0f5f15d6623b4ffcfaf13b1e61557944fa1f114
Joerg Weingrill <jweingrill@aip.de>
2016-05-20 18:38:05 +0200

    ngc2281: new planwifsip and introduced config file

--- src/ngc2281/planwifsip.py
+++ src/ngc2281/planwifsip.py
@@ -1,51 +1,34 @@
 '''
 Created on Apr 12, 2013
+Updated May 20, 2016
 
 @author: jwe <jweingrill@aip.de>
 '''
+import config
+from opencluster import OpenCluster
+from clustergroup import ClusterGroup
 
 def do_rot(transfer=False):
-    from opencluster import OpenCluster
+    import datetime
 
-    ngc2281 = OpenCluster(objectname='NGC 2281', 
-                          uname='NGC2281 rot', 
-                          obsmode='rot')
-    ngc2281.title = 'SOCS'
-    ngc2281.abstract = 'Photometric monitoring of open stellar clusters'
-    ngc2281_subframes = ngc2281.plan_wifsip(nfields=4)
+    ngc2281rot = OpenCluster(objectname='NGC 2281', uname='NGC 2281 rot', obsmode='rot')
+    
+    ngc2281rot.title = 'SOCS'
+    ngc2281rot.abstract = 'Photometric monitoring of open stellar clusters'
+    ngc2281rot_subframes = ngc2281rot.plan_wifsip(nfields=4)
+    ngc2281rot_group = ClusterGroup(ngc2281rot)
+    ngc2281rot_group.startdate =  datetime.datetime(2016, 8, 20) 
+    ngc2281rot_group.enddate =  datetime.datetime(2017, 5, 13)
+    
+    for sf in ngc2281rot_subframes:
+        print sf.uname, sf.duration
+        sf.tofile(config.projectpath)
+        ngc2281rot_group.add_daughter(sf.uname)
+        if transfer: 
+            sf.transfer()
+    ngc2281rot_group.tofile(config.projectpath)
+    if transfer: 
+        ngc2281rot_group.transfer()
 
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_aspect(1.)
-
-    for sf in ngc2281_subframes:
-        print sf.uname
-        sf.plot(ax)
-        sf.tycho()
-        sf.tofile('/work2/jwe/ngc2281')
-        #sf.transfer()
-    xmin,xmax = plt.xlim()
-    plt.xlim(xmax,xmin)
-    plt.show()
-
-def do_bvi(transfer=False):
-    from opencluster import OpenCluster
-
-    ngc2281 = OpenCluster(objectname='NGC 2281', 
-                          uname='NGC 2281 BVI', 
-                          obsmode='bvisl')
-    ngc2281.title = 'SOCS'
-    ngc2281.abstract = 'Photometric monitoring of open stellar clusters'
-    ngc2281_subframes = ngc2281.plan_wifsip(nfields=5)
-    ngc2281.mode['timeout'] = 5030000.0 # redoing two fields in BVI
-    for sf in ngc2281_subframes:
-        print sf.uname
-        sf.tofile('/work2/jwe/ngc2281')
-        sf.transfer()
-        
 if __name__ == '__main__':
-    import matplotlib
-    matplotlib.use('WXAgg')
-    #do_rot(transfer=False)
-    do_bvi(transfer=True)
+    do_rot(transfer=True)
