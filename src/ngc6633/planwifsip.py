8cd3a25e769f825af5c15960ed07f6bf2a643770
Joerg Weingrill <jweingrill@aip.de>
2017-10-04 10:14:56 +0200

    changing paths to respect config file

--- src/ngc6633/planwifsip.py
+++ src/ngc6633/planwifsip.py
@@ -4,6 +4,8 @@ Created on Apr 12, 2013
 @author: jwe <jweingrill@aip.de>
 '''
 
+import config
+
 def do_rot(transfer=False):
     from opencluster import OpenCluster
 
@@ -23,28 +25,27 @@ def do_rot(transfer=False):
         print sf.uname
         sf.plot(ax)
         sf.tycho()
-        sf.tofile('/work2/jwe/NGC6633')
+        sf.tofile(config.projectpath+'submit/')
         if transfer:
             sf.transfer()
     xmin,xmax = plt.xlim()
     plt.xlim(xmax,xmin)
     #plt.show()
-    plt.savefig('/work2/jwe/NGC6633/ngc6633.pdf')
+    plt.savefig(config.plotpath+'ngc6633submit.pdf')
     plt.close()
 
 def do_bvi(transfer=False):
     from opencluster import OpenCluster
 
     ngc6633 = OpenCluster(objectname='NGC 6633', 
-                          uname='NGC 6633 BVI', 
-                          obsmode='bvisl')
-    ngc6633.title = 'SOCS'
-    ngc6633.abstract = 'Photometric monitoring of open stellar clusters'
+                          obsmode='BVI')
+    ngc6633.startdate = '2017-08-25'
+    ngc6633.enddate = '2017-09-08'
     ngc6633_subframes = ngc6633.plan_wifsip(nfields=5)
-    ngc6633.mode['timeout'] = 5030000.0 # redoing two fields in BVI
+    #ngc6633.mode['timeout'] = 5030000.0 # redoing two fields in BVI
     for sf in ngc6633_subframes:
         print sf.uname
-        sf.tofile('/work2/jwe/NGC6633')
+        sf.tofile(config.projectpath+'submit/')
         if transfer:
             sf.transfer()
         
@@ -52,4 +53,4 @@ if __name__ == '__main__':
     import matplotlib
     matplotlib.use('WXAgg')
     #do_rot(transfer=True)
-    do_bvi(transfer=True)
+    do_bvi(transfer=False)
