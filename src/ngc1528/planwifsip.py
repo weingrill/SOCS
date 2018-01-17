b1ca64555657647663926730c8e108cf2c1a6430
Joerg Weingrill <jweingrill@aip.de>
2018-01-17 12:09:18 +0100

    changed startdate of observation

--- src/ngc1528/planwifsip.py
+++ src/ngc1528/planwifsip.py
@@ -19,7 +19,7 @@ def do_rot(transfer=False):
     ngc1528rot.abstract = 'Photometric monitoring of open stellar clusters'
     ngc1528rot_subframes = ngc1528rot.plan_wifsip(nfields=4)
     ngc1528rot_group = ClusterGroup(ngc1528rot)
-    ngc1528rot_group.startdate =  datetime.datetime(2017, 7, 16) 
+    ngc1528rot_group.startdate =  datetime.datetime(2017, 10, 27) 
     ngc1528rot_group.enddate =  datetime.datetime(2018, 4, 14)
     
     for sf in ngc1528rot_subframes:
