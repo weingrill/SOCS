df2f84549446cea068a4be67f60de53a6cd3bf7e
Joerg Weingrill <jweingrill@aip.de>
2018-02-20 16:56:39 +0100

    sciencedemo: initial commit

---
+++ src/m67/sciencedemo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on Jan 26, 2018
+
+@author: Joerg Weingrill <jweingrill@aip.de>
+'''
+import astromatic_wrapper as aw
+
+class ScienceDemo(object):
+    '''
+    classdocs
+    see http://astromatic-wrapper.readthedocs.io/en/latest/index.html
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def makecmd(self):
+        pass
+    
+    def makeccd(self):
+        pass
+        
+if __name__ == '__main__':
+    sc = ScienceDemo()
