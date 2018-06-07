91aef969eec4432d5affd2e53d756915c2c3e069
hg <hg@localhost>
2018-06-07 09:25:19 +0200

    added BVR and uvby observation

--- src/ngc6940/planwifsip.py
+++ src/ngc6940/planwifsip.py
@@ -14,7 +14,7 @@ end   2016-12-15 jd = 2457752.5
 25.05.2016 18:30:32 [WARN] DOMTarget.parse…:280 No image type specified
 
 '''
-import config
+import config  # @UnresolvedImport
 from opencluster import OpenCluster
 from clustergroup import ClusterGroup
 
@@ -42,6 +42,33 @@ def do_rot(transfer=False):
     if transfer: 
         ngc6940rot_group.transfer()
 
+def do_bvr(transfer=False):
+    ngc6940bvr = OpenCluster(objectname='NGC 6940', 
+                          obsmode='BVR')
+    ngc6940bvr.startdate = '2018-06-06'
+    ngc6940bvr.enddate = '2018-07-05'
+    ngc6940bvr_subframes = ngc6940bvr.plan_wifsip(nfields=5)
+    for sf in ngc6940bvr_subframes:
+        print sf.uname
+        sf.tofile(config.projectpath+'submit/')
+        if transfer:
+            sf.transfer()
+
+def do_uvby(transfer=False):
+    ngc6940uvby = OpenCluster(objectname='NGC 6940', 
+                          obsmode='uvby')
+    ngc6940uvby.startdate = '2018-06-06'
+    ngc6940uvby.enddate = '2018-07-05'
+    ngc6940uvby_subframes = ngc6940uvby.plan_wifsip(nfields=5)
+    for sf in ngc6940uvby_subframes:
+        print sf.uname
+        sf.tofile(config.projectpath+'submit/')
+        if transfer:
+            sf.transfer()
+
+
         
 if __name__ == '__main__':
-    do_rot(transfer=True)
+    #do_rot(transfer=True)
+    #do_bvr(transfer=True)
+    do_uvby(transfer=True)
