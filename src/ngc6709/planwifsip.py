cdd6a2dc640be58b9df55a5cff5c5456b2898e86
hg <hg@localhost>
2018-06-07 09:24:35 +0200

    added rottest

--- src/ngc6709/planwifsip.py
+++ src/ngc6709/planwifsip.py
@@ -5,7 +5,7 @@ Created on Apr 12, 2013
 
 @author: jwe <jweingrill@aip.de>
 '''
-import config
+import config  # @UnresolvedImport
 from opencluster import OpenCluster
 
 def do_rot(cluster, transfer=False):
@@ -15,6 +15,15 @@ def do_rot(cluster, transfer=False):
     ngc6709.tofile(config.projectpath)
     if transfer:
         ngc6709.transfer()
+ 
+def do_rottest(cluster, transfer=False):
+    ngc6709 = OpenCluster(objectname = cluster, obsmode = 'rottest')
+    ngc6709.title = 'SOCS'
+    ngc6709.abstract = 'Photometric monitoring of open stellar clusters'
+    ngc6709.tofile(config.projectpath)
+    if transfer:
+        ngc6709.transfer()
+
             
 def do_bvr(cluster, transfer=False):
 
@@ -26,5 +35,6 @@ def do_bvr(cluster, transfer=False):
         ngc6709.transfer()
         
 if __name__ == '__main__':
-    do_rot('NGC 6709', transfer=True)
+    #do_rot('NGC 6709', transfer=True)
+    do_rottest('NGC 6709', transfer=True)
     #do_bvr('NGC 6709', transfer=True)
