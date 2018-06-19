b1d23ce0632771e51975b57ab4f0ddc98c600a98
hg <hg@localhost>
2018-06-19 16:30:20 +0200

    cleaning up unused parameters and added frot

--- src/ngc6709/planwifsip.py
+++ src/ngc6709/planwifsip.py
@@ -10,16 +10,19 @@ from opencluster import OpenCluster
 
 def do_rot(cluster, transfer=False):
     ngc6709 = OpenCluster(objectname = cluster, obsmode = 'rot')
-    ngc6709.title = 'SOCS'
-    ngc6709.abstract = 'Photometric monitoring of open stellar clusters'
     ngc6709.tofile(config.projectpath)
     if transfer:
         ngc6709.transfer()
+
+def do_frot(cluster, transfer=False):
+    ngc6709frot = OpenCluster(objectname = cluster, obsmode = 'frot')
+    ngc6709frot.tofile(config.projectpath)
+    if transfer:
+        ngc6709frot.transfer()
+ 
  
 def do_rottest(cluster, transfer=False):
     ngc6709 = OpenCluster(objectname = cluster, obsmode = 'rottest')
-    ngc6709.title = 'SOCS'
-    ngc6709.abstract = 'Photometric monitoring of open stellar clusters'
     ngc6709.tofile(config.projectpath)
     if transfer:
         ngc6709.transfer()
@@ -28,13 +31,12 @@ def do_rottest(cluster, transfer=False):
 def do_bvr(cluster, transfer=False):
 
     ngc6709 = OpenCluster(objectname = cluster, obsmode='BVR')
-    ngc6709.title = 'SOCS'
-    ngc6709.abstract = 'Photometric monitoring of open stellar clusters'
     ngc6709.tofile(config.projectpath)
     if transfer:
         ngc6709.transfer()
         
 if __name__ == '__main__':
     #do_rot('NGC 6709', transfer=True)
-    do_rottest('NGC 6709', transfer=True)
+    do_frot('NGC 6709', transfer=True)
+    #do_rottest('NGC 6709', transfer=True)
     #do_bvr('NGC 6709', transfer=True)
